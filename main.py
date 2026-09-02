ff4d338a41111433ae9bbc254b9d450472cb7a9f
ksazzad00039-web <ksazzad00039@gmail.com>
2026-09-02 23:28:36 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-
 import os
+from flask import Flask
+
+app = Flask(__name__)
+
+# আপনার টেলিগ্রাম বট বা অন্যান্য কোড এখানে থাকবে...
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
